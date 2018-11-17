fab634d7b45334271792f3728a98e1d373fdb752
R <ryomarcher@gmail.com>
2018-11-16 23:58:09 -0500

    Now CountVecotrizer object can transform multiple documents. Next to implement is conversion of Vec<HashMap<int, int>> to Vec<Vec<int>> or two dimensional array.

--- src/lib.rs
+++ src/lib.rs
@@ -4,61 +4,68 @@ use std::vec::Vec;
 use std::collections::HashMap;
 use regex::Regex;
 
+
 pub struct CountVectorizer<'a> {
     pub vocabulary_ : HashMap<&'a str, i32>,
-    pub vocabulary_counts_ : HashMap<i32, i32>,
 }
 
 impl<'a> CountVectorizer<'a> {
     pub fn new() -> CountVectorizer<'a> {
         let map: HashMap<&'a str, i32> = HashMap::new();
-        let vocab_counts: HashMap<i32, i32> = HashMap::new();
+
+        // Return a new instance
         CountVectorizer {
             vocabulary_: map,
-            vocabulary_counts_: vocab_counts,
         }
     }
 
-    pub fn fit(&mut self, doc: &'a str) {
-        // Tokenize the doc
+    fn _tokenize_single_doc(doc: &str) -> Vec<&str> {
         let token_pattern=r"(?u)\b\w\w+\b";
         let _re = Regex::new(token_pattern).unwrap();
         let _tokens: Vec<&str> = _re.find_iter(doc)
             .map(|f| f.as_str())
             .collect();
-        
+        _tokens
+    }
+
+    fn _tokenize_multiple_docs(docs: Vec<&str>) -> Vec<Vec<&str>> {
+        let mut _tokenized_docs: Vec<Vec<&str>> = Vec::new();
+        for doc in docs {
+            let mut _tokens: Vec<&str> ;
+            _tokens = CountVectorizer::_tokenize_single_doc(doc);
+            _tokenized_docs.push(_tokens)
+        };
+        _tokenized_docs
+    }
+
+    pub fn fit_transform(&mut self, docs: Vec<&'a str>) -> Vec<HashMap<i32, i32>> {
+        // tokenize the document collection
+        let _tokenized_docs = CountVectorizer::_tokenize_multiple_docs(docs);
+
+        // Vec to store vocab. count HashMap. Variable to return.
+        let mut X: Vec<HashMap<i32, i32>> = Vec::new();
+
         // Collect vocabulary
-        let mut counter: i32 = 1;
-        for _token in _tokens {
-            if !self.vocabulary_.contains_key(_token) {
-                self.vocabulary_.insert(_token, counter.clone());
-                counter = counter + 1;
+        let mut vocab_indexer: i32 = 1;             // indexer for unique words
+
+        for _tokenized_doc in _tokenized_docs {
+            // HashMap to store vocab. counts for a doc
+            let mut _vocab_counts: HashMap<i32, i32> = HashMap::new();
+
+            for _token in _tokenized_doc {
+                // if _token is a new word, add to vocabulary_ and vocabulary_counts_
+                if !self.vocabulary_.contains_key(_token) {
+                    self.vocabulary_.insert(_token, vocab_indexer.clone());
+                    _vocab_counts.insert(vocab_indexer, 1);
+                    vocab_indexer = vocab_indexer + 1;
+                } else {        // Otherwise add vocab counts
+                    let vocab_ind = self.vocabulary_[_token];
+                    *(_vocab_counts).get_mut(&vocab_ind).unwrap() += 1;
+                };
             }
+            X.push(_vocab_counts);
         }
+        X   // Return the Vec of count HashMaps
     }
-
-    pub fn fit_transform(&mut self, doc: &'a str) {
-        // Tokenize the doc
-        let token_pattern=r"(?u)\b\w\w+\b";
-        let _re = Regex::new(token_pattern).unwrap();
-        let _tokens: Vec<&str> = _re.find_iter(doc)
-            .map(|f| f.as_str())
-            .collect();
-        
-        // Collect vocabulary
-        let mut vocab_indexer: i32 = 1;
-        for _token in _tokens {
-            // if _token is a new word, add to vocabulary_ and vocabulary_counts_
-            if !self.vocabulary_.contains_key(_token) {
-                self.vocabulary_.insert(_token, vocab_indexer.clone());
-                self.vocabulary_counts_.insert(vocab_indexer, 1);
-
-                vocab_indexer = vocab_indexer + 1;
-            } else { // Otherwise add vocab counts
-                let vocab_ind = self.vocabulary_[_token];
-                *(self.vocabulary_counts_).get_mut(&vocab_ind).unwrap() += 1;
-            };
-        }
-    }
-
 }
+
--- src/main.rs
+++ src/main.rs
@@ -2,88 +2,25 @@
 extern crate regex;
 extern crate vectorizer;
 
-use std::vec::Vec;
-use std::collections::HashMap;
-use regex::Regex;
+// use std::vec::Vec;
+// use std::collections::HashMap;
+// use regex::Regex;
 use vectorizer:: CountVectorizer;
 
 fn main() {
 
-    fn _tokenize(doc: &str) -> Vec<&str> {
-        let token_pattern=r"(?u)\b\w\w+\b";
-        let _re = Regex::new(token_pattern).unwrap();
-        let _tokens: Vec<&str> = _re.find_iter(doc)
-            .map(|f| f.as_str())
-            .collect();
-        _tokens
-    };
-
-    fn _build_vocabulary(tokens: Vec<&str>) -> HashMap<&str, i32> {
-        let mut vocabulary = HashMap::new();
-        let mut counter: i32 = 1;
-        for token in tokens {
-            // println!("Token#{:?}: {:?}", &counter, &token);
-            if !vocabulary.contains_key(token) {
-                vocabulary.insert(token.clone(), counter.clone());
-                counter = counter + 1;
-            }
-        };
-        vocabulary
-    }
+    let fruits_str = "apple, banana, apple, banana, orange, \
+                      apple. apple, banana, orange, orange";
+    let numbers_str = "one, two, three, two, three, three. three, four, four, ONE";
+    let mut docs: Vec<&str> = Vec::new();
+    docs.push(fruits_str);
+    docs.push(numbers_str);
+    //println!("{:?}", docs);
     
-    fn _build_vocabulary_and_count(tokens: Vec<&str>) -> HashMap<i32, i32> {
-        let vocabulary = _build_vocabulary(tokens.clone());
+    // Testing CountVectorizer
+    let mut vectorizer = CountVectorizer::new();
 
-        let mut vocab_count = HashMap::new();
-
-        for token in tokens {
-            let token_ind = vocabulary[token];
-            if !vocab_count.contains_key(&token_ind) {
-                vocab_count.insert(token_ind, 1);
-            }
-            else {
-                *vocab_count.get_mut(&token_ind).unwrap() += 1;
-            };
-        };
-        // println!("{:?}", vocab_count);
-        vocab_count
-    };
-
-    let fruits_str = "apple, banana, apple, banana, orange, apple.";
-    let numbers_str = "one, two, three, two, three, three.";
-
-    let tokens1 = _tokenize(fruits_str.clone());
-    let tokens2 = _tokenize(numbers_str.clone());
-    println!("=== parsing fruits ===");
-    println!("Tokenizer: {:?}", tokens1);
-    println!("Vocabulary: {:?}", _build_vocabulary(tokens1.clone()));
-    println!("Vocabulary Counts: {:?}\n", _build_vocabulary_and_count(tokens1));
-
-    println!("=== parsing numbers ===");
-    println!("Tokenizer: {:?}", tokens2);
-    println!("Vocabulary: {:?}", _build_vocabulary(tokens2.clone()));
-    println!("Vocabulary Counts: {:?}\n", _build_vocabulary_and_count(tokens2));
-
-
-    let mut vectorizer1 = CountVectorizer::new();
-    let mut vectorizer2 = CountVectorizer::new();
-    vectorizer1.fit(fruits_str);
-    vectorizer2.fit(numbers_str);
-    println!("=== fit() with Vectorizer ===");
-    println!("Vectorizer Vocabulary: {:?}", vectorizer1.vocabulary_);
-    println!("Vectorizer Vocabulary: {:?}\n", vectorizer2.vocabulary_);
-
-    let mut vectorizer3 = CountVectorizer::new();
-    let mut vectorizer4 = CountVectorizer::new();
-    vectorizer3.fit_transform(fruits_str);
-    vectorizer4.fit_transform(numbers_str);
-    println!("=== fit_transform() with Vectorizer ===");
-    println!("Vectorizer Vocabulary: {:?}, Counts:{:?}", 
-            vectorizer3.vocabulary_, vectorizer3.vocabulary_counts_);
-    println!("Vectorizer Vocabulary: {:?}, Counts:{:?}\n", 
-            vectorizer4.vocabulary_, vectorizer4.vocabulary_counts_);
-
-    //fn _count_vocab(tokens: &Vec) -> vocabulary: HashSet<&str> {};
-        // count number of appearances of each word in the token
-
+    let X = vectorizer.fit_transform(docs);
+    println!("(word_id: count) :{:?}", X);
+    println!("(Word: word_id) : {:?}", vectorizer.vocabulary_);
 }
