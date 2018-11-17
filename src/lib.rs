0a271fa82645792813a5f6e0d1b1eaf59e97f29c
R <ryomarcher@gmail.com>
2018-11-17 11:22:01 -0500

    _sort_vocabulary_count() incorporated into fit_transform(). A new test case revealed a bug where a word is already in the vocabulary from a previously processed doc, but it's not encountered yet in the current doc, which gave error for calling unwrap() on None. The code was replaced by .entry().or_insert() and it's functioning. It may be a good idea to consider use of IndexMap for counting vocabulary for each doc, which may or may not make downstream sorting faster.

--- src/lib.rs
+++ src/lib.rs
@@ -41,30 +41,20 @@ impl<'a> CountVectorizer<'a> {
         _tokenized_docs
     }
 
-    pub fn _sort_vocabulary_count(&self, X: Vec<HashMap<i32, i32>>) -> Array2<i32>{
+    fn _sort_vocabulary_count(&self, X: Vec<HashMap<i32, i32>>) -> Array2<i32>{
         let num_rows = X.len();
         let num_columns = self.vocabulary_.len();
-        let array = Array2::<i32>::zeros((num_rows, num_columns));
+        let mut sorted_X = Array2::<i32>::zeros((num_rows, num_columns));
 
         for i in 0..num_rows {
-            println!("=== Sample{:?} ====", i);
-            println!("Sample = {:?}", X[i]);
-            for j in 0..num_columns {
-                match X[i].get(&(j as i32)) {
-                    Some(v) => {
-                        println!("Count for {:?}: {:?}", j, v);
-                    },
-                    None => {
-                        println!("Key {:?} is not in sample{:?}", j, i);
-                    },
-                }
+            for key in X[i].keys() {
+                sorted_X[[i, (*key as usize)]] = *X[i].get(key).unwrap();
             }
-            println!("\n");
         }
-        array
+        sorted_X
     }
 
-    pub fn fit_transform(&mut self, docs: Vec<&'a str>) -> Vec<HashMap<i32, i32>> {
+    pub fn fit_transform(&mut self, docs: Vec<&'a str>) -> Array2<i32> { //Vec<HashMap<i32, i32>> {
         // tokenize the document collection
         let _tokenized_docs = CountVectorizer::_tokenize_multiple_docs(docs);
 
@@ -74,54 +64,25 @@ impl<'a> CountVectorizer<'a> {
         // Collect vocabulary
         let mut vocab_indexer: i32 = 0;             // indexer for unique words
 
-        for _tokenized_doc in _tokenized_docs {
+        for _doc in _tokenized_docs {
             // HashMap to store vocab. counts for a doc
             let mut _vocab_counts: HashMap<i32, i32> = HashMap::new();
 
-            for _token in _tokenized_doc {
+            for _token in _doc {
                 // if _token is a new word, add to vocabulary_ and vocabulary_counts_
                 if !self.vocabulary_.contains_key(_token) {
                     self.vocabulary_.insert(_token, vocab_indexer.clone());
-                    _vocab_counts.insert(vocab_indexer, 1);
+                    _vocab_counts.insert(vocab_indexer.clone(), 1);
                     vocab_indexer = vocab_indexer + 1;
                 } else {        // Otherwise add vocab counts
                     let vocab_ind = self.vocabulary_[_token];
-                    *(_vocab_counts).get_mut(&vocab_ind).unwrap() += 1;
+                    *(_vocab_counts).entry(vocab_ind).or_insert(0) += 1;
                 };
             }
             X.push(_vocab_counts);
         }
-        X   // Return the Vec of count HashMaps
-    }
-
-    pub fn fit_transform2(&mut self, docs: Vec<&'a str>) -> Vec<IndexMap<i32, i32>> {
-        // tokenize the document collection
-        let _tokenized_docs = CountVectorizer::_tokenize_multiple_docs(docs);
-
-        // Vec to store vocab. count HashMap. Variable to return.
-        let mut X: Vec<IndexMap<i32, i32>> = Vec::new();
-
-        // Collect vocabulary
-        let mut vocab_indexer: i32 = 1;             // indexer for unique words
-
-        for _tokenized_doc in _tokenized_docs {
-            // HashMap to store vocab. counts for a doc
-            let mut _vocab_counts: IndexMap<i32, i32> = IndexMap::new();
-
-            for _token in _tokenized_doc {
-                // if _token is a new word, add to vocabulary_ and vocabulary_counts_
-                if !self.vocabulary_.contains_key(_token) {
-                    self.vocabulary_.insert(_token, vocab_indexer.clone());
-                    _vocab_counts.insert(vocab_indexer, 1);
-                    vocab_indexer = vocab_indexer + 1;
-                } else {        // Otherwise add vocab counts
-                    let vocab_ind = self.vocabulary_[_token];
-                    *(_vocab_counts).get_mut(&vocab_ind).unwrap() += 1;
-                };
-            }
-            X.push(_vocab_counts);
-        }
-        X   // Return the Vec of count HashMaps
+        let sortedX = self._sort_vocabulary_count(X);
+        sortedX   // Return the Vec of count HashMaps
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ extern crate regex;
 extern crate vectorizer;
 
 // use std::vec::Vec;
-// use std::collections::HashMap;
+use std::collections::HashMap;
 // use regex::Regex;
 use vectorizer:: CountVectorizer;
 
 fn main() {
 
-    let fruits_str = "apple, banana, apple, banana, orange, \
-                      apple. apple, banana, orange, orange";
-    let numbers_str = "one, two, three, two, three, three. three, four, four, ONE";
+    let fruits_str = "apple, banana, apple, banana, orange, three, \
+                      apple. apple, banana, orange, orange, ONE, three";
+    let numbers_str = "one, two, three, two, three, apple, three. three, four, four, ONE";
     let mut docs: Vec<&str> = Vec::new();
     docs.push(fruits_str);
     docs.push(numbers_str);
@@ -19,13 +19,27 @@ fn main() {
     
     // Testing CountVectorizer
     let mut vectorizer = CountVectorizer::new();
-
-    let X = vectorizer.fit_transform(docs);
-    println!("(word_id: count) :{:?}", X);
-    println!("(Word: word_id) : {:?}", vectorizer.vocabulary_);
+    let x = vectorizer.fit_transform(docs);
+    
+    // Print original docs
+    println!("Doc0 :{:?}", fruits_str);
+    println!("Doc1 :{:?}", numbers_str);
     println!("\n");
 
-    let array = vectorizer._sort_vocabulary_count(X);
-    println!("{:?}", array);
+    // print Word_id: Word correspondence
+    let vocab_size = vectorizer.vocabulary_.len();
+    let mut vocabulary_inverted: HashMap<i32, &str> = HashMap::new();
+    for (k, v) in vectorizer.vocabulary_.iter() {
+        vocabulary_inverted.insert(*v, k);
+    }
+    for i in 0..vocab_size {
+        println!("(Word_id: Word) : ({:?}:{:?})", i, vocabulary_inverted[&(i as i32)]);
+    }
+    
+    println!("CountVector :\n{:?}", x);
+    println!("\n");
+
+    // let array = vectorizer._sort_vocabulary_count(X);
+    // println!("{:?}", array);
 
 }
